1f728aed01c0a07bc54f86cbaf5db4557f3aa78d
Jorge Santos <neneluis55@gmail.com>
2019-12-29 09:22:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -51,10 +51,10 @@ def nyc_pigeon_organizer (data)
       new_hash[name][key] = [] 
     end 
     
-  new_hash[name][key] << new_value
+  new_hash[name][key] << new_value.to_s
 end 
 end 
 end 
-new_hash
+ return new_hash
   end
 
